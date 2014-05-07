aad82813404f9ee9e80812e95ac6de4ce8ca5e90
Jay Marcyes <jay@marcyes.com>
2014-05-06 17:17:09 -0700

    bah, updating setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import ast
 
 name = 'pyt'
 version = ''
-with open('{}.py'.format(name), 'rU') as f:
+with open('{}/__init__.py'.format(name), 'rU') as f:
     for node in (n for n in ast.parse(f.read()).body if isinstance(n, ast.Assign)):
         node_name = node.targets[0]
         if isinstance(node_name, ast.Name) and node_name.id.startswith('__version__'):
@@ -26,7 +26,7 @@ setup(
     author='Jay Marcyes',
     author_email='jay@marcyes.com',
     url='http://github.com/Jaymon/{}'.format(name),
-    py_modules=[name],
+    packages=[name],
     license="MIT",
     classifiers=[ # https://pypi.python.org/pypi?:action=list_classifiers
         'Development Status :: 4 - Beta',
